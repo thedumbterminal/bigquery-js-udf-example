a9d774441f91dbb104b85b383de73466bedaaaee
thedumbterminal <github@thedumbterminal.co.uk>
2021-04-01 21:55:39 +0100

    added deploy script

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,23 +1,35 @@
-#!/usr/bin/env node
-
-const {Storage} = require('@google-cloud/storage')
+const path = require('path')
+const { Storage } = require('@google-cloud/storage')
 
 const storage = new Storage()
 
-const createBucketIfNeeded = async (bucket) => {
-	await storage.createBucket(bucket)
+const createBucketIfNeeded = async (bucketName) => {
+  try {
+    await storage.createBucket(bucketName)
+  } catch (e) {
+    if (e.message !== 'Sorry, that name is not available. Please try a different one.') throw e
+  }
+  return storage.bucket(bucketName)
+}
+
+const uploadFile = (bucket, dest, source) => {
+  const destination = path.join(dest, source)
+  return bucket.upload(source, {
+    destination
+  })
 }
 
 const upload = async () => {
-	const bucket = process.env.DEST_BUCKET || 'thedumbterminal/bigquery-udf-example'
-	await createBucketIfNeeded(bucket)
+  const bucketName = process.env.DEST_BUCKET
+  const bucket = await createBucketIfNeeded(bucketName)
+  await uploadFile(bucket, 'bigquery-js-udf-example', 'dist/dist.js')
 }
 
 upload()
-.catch((e) => {
-	console.error(e.message)
-	process.exit(1)
-})
-.then(() => {
-	console.log('...Success')
-})
+  .catch((e) => {
+    console.error(e.message)
+    process.exit(1)
+  })
+  .then(() => {
+    console.log('...Success')
+  })
